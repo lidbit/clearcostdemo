e098ffa1bc0a3a74ffbc1d37eaccd063d419ab93
Lydia Douglas <lydia.d.1001@gmail.com>
2022-11-24 12:22:04 +1000

    Added mock file.

---
+++ src/app/data/mock-tech-kpis.ts
@@ -0,0 +1,70 @@
+export const mockTechKpis = [
+    {
+      ProductID: 1,
+      ProductName: "IT Budget as % of Org Budget",
+      SupplierID: 11.9,
+      CategoryID: 1,
+      QuantityPerUnit: "11.09%",
+      UnitPrice: 18.0,
+      UnitsInStock: 39,
+      UnitsOnOrder: 0,
+      ReorderLevel: 10,
+      Discontinued: false,
+      Category: {
+        CategoryID: 1,
+        CategoryName: "Beverages",
+        Description: "Soft Fdrinks, coffees, teas, beers, and ales"
+      }
+    },
+    {
+      ProductID: 3,
+      ProductName: "IT Staff as % of Total Org Staff",
+      SupplierID: 2.08,
+      CategoryID: 2,
+      QuantityPerUnit: "2.08%",
+      UnitPrice: 10.0,
+      UnitsInStock: 13,
+      UnitsOnOrder: 70,
+      ReorderLevel: 25,
+      Discontinued: false,
+      Category: {
+        CategoryID: 2,
+        CategoryName: "Condiments",
+        Description: "Sweet and savory sauces, relishes, spreads, and seasonings"
+      }
+    },
+    {
+      ProductID: 4,
+      ProductName: "IT Budget as % of Org Revenue",
+      SupplierID: 1.11,
+      CategoryID: 2,
+      QuantityPerUnit: "1.11%",
+      UnitPrice: 22.0,
+      UnitsInStock: 53,
+      UnitsOnOrder: 0,
+      ReorderLevel: 0,
+      Discontinued: false,
+      Category: {
+        CategoryID: 2,
+        CategoryName: "Condiments",
+        Description: "Sweet and savory sauces, relishes, spreads, and seasonings"
+      }
+    },
+    {
+      ProductID: 5,
+      ProductName: "IT Cost Per User",
+      SupplierID: 1108.00,
+      CategoryID: 2,
+      QuantityPerUnit: "$1108.00",
+      UnitPrice: 21.35,
+      UnitsInStock: 0,
+      UnitsOnOrder: 0,
+      ReorderLevel: 0,
+      Discontinued: true,
+      Category: {
+        CategoryID: 2,
+        CategoryName: "Condiments",
+        Description: "Sweet and savory sauces, relishes, spreads, and seasonings"
+      }
+    }
+  ];
